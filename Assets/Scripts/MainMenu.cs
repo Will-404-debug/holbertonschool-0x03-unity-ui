fe1f9c888fb0fdd9b0a2d3c26b8554650d90c089
William <wilguidro@gmail.com>
2025-01-20 19:27:59 +0100

    Updated the MainMenu so that it is now possible to quit the game

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,4 +9,14 @@ public class MainMenu : MonoBehaviour
         // Load the maze scene (you should have the maze scene added to Build Settings)
         SceneManager.LoadScene("maze"); // Ensure that "maze" is the name of your maze scene
     }
+
+    // Method called when the Quit button is pressed
+    public void QuitMaze()
+    {
+        #if UNITY_EDITOR
+        Debug.Log("Quit Game");  // Log message in the Unity editor
+        #else
+        Application.Quit(); // Close the game in a build
+        #endif
+    }
 }
