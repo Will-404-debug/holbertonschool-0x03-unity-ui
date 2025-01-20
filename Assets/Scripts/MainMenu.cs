47348e6ed79a5aed077044859a5549a3389ab6a0
William <wilguidro@gmail.com>
2025-01-20 20:20:38 +0100

    Added the Colorblind Mode

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,22 +1,31 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement; // Required for scene management
+using UnityEngine.UI; // Required for Toggle component
 
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat; // Material for the trap
+    public Material goalMat; // Material for the goal
+    public Toggle colorblindMode; // Toggle for colorblind mode
+
     // Method called when the Play button is pressed
     public void PlayMaze()
     {
-        // Load the maze scene (you should have the maze scene added to Build Settings)
-        SceneManager.LoadScene("maze"); // Ensure that "maze" is the name of your maze scene
-    }
+        // Check if colorblind mode is enabled
+        if (colorblindMode.isOn)
+        {
+            // Change the trap and goal colors for colorblind mode
+            trapMat.color = new Color32(255, 112, 0, 255); // Orange for traps
+            goalMat.color = Color.blue; // Blue for the goal
+        }
+        else
+        {
+            // Set the original colors for traps and goal
+            trapMat.color = Color.red; // Red for traps
+            goalMat.color = Color.green; // Green for the goal
+        }
 
-    // Method called when the Quit button is pressed
-    public void QuitMaze()
-    {
-        #if UNITY_EDITOR
-        Debug.Log("Quit Game");  // Log message in the Unity editor
-        #else
-        Application.Quit(); // Close the game in a build
-        #endif
+        // Load the maze scene (you should have the maze scene added to Build Settings)
+        SceneManager.LoadScene("maze");
     }
 }
